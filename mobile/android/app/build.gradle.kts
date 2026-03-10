634d47104208267dfcf45eb5d78f6947e77633b3
Siva Kumar Perumalla <sivakumar.perumalla@gmail.com>
2026-03-10 22:14:30 +0530

    chore(android): temporarily bypass NDK version requirement for local dev

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.zests.zests_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
